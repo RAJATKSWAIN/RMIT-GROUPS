91320639655f9ddfafed5c0af0ffea20a53ae756
RAJAT KUMAR SWAIN <145832002+RAJATKSWAIN@users.noreply.github.com>
2026-02-24 00:15:41 +0530

    Update sidebar.php

--- fees-system/admin/layout/sidebar.php
+++ fees-system/admin/layout/sidebar.php
@@ -96,8 +96,8 @@ html, body {
     <!-- Icon + Title -->
     <i class="bi bi-cash-stack fs-2 text-primary mb-0"></i>
     <h6 class="text-white mb-0">Fees Management System</h6>
-    <small style="background-color: skyblue; color: white; padding: 2px 6px; border-radius: 4px; font-weight:700px;">
-  		Ver - 1.0.0
+    <small class="text-white" style="border:1px solid skyblue; padding:2px 6px; border-radius:4px; front-weight:700px;">
+		  Ver 1.0.0
 	</small>
   </div>
 

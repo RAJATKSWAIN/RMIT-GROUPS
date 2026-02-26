11dbc9561c0e0cc0585c5cc5a96b808095b044f2
RAJAT KUMAR SWAIN <145832002+RAJATKSWAIN@users.noreply.github.com>
2026-02-26 13:03:58 +0530

    Update file name reference in edit.php

--- fees-system/admin/students/edit.php
+++ fees-system/admin/students/edit.php
@@ -1,5 +1,5 @@
 <!--======================================================
-    File Name   : disable.php
+    File Name   : edit.php
     Project     : RMIT Groups - FMS - Fees Management System
     Module      : STUDENT MANAGEMENT
     Description : Student Registration & Profile Management

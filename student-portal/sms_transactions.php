c9aa1f95dab1ddcdb53f4daa1abe5720c5e3209a
RAJAT KUMAR SWAIN <145832002+RAJATKSWAIN@users.noreply.github.com>
2026-02-26 12:20:02 +0530

    Revise header comments in sms_transactions.php
    Updated file header comments with project details.

--- student-portal/sms_transactions.php
+++ student-portal/sms_transactions.php
@@ -1,3 +1,12 @@
+<!--======================================================
+    File Name   : sms_transactions.php
+    Project     : RMIT Groups - FMS - Fees Management System
+    Description : SMS - Student Management Portal | Transaction Page
+    Developed By: TrinityWebEdge
+    Date Created: 25-02-2026
+    Last Updated: 26-02-2026
+    Note        : This page defines the SMS - Student Management Portal | Dashboard Page.
+=======================================================-->
 <?php
 require_once __DIR__ . '/../fees-system/config/db.php';
 require_once __DIR__ . '/../fees-system/config/config.php';
